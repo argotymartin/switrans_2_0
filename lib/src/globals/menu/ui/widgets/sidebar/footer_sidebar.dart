6c2d3490880bea8d5c985adc5d7dfcc2311fdf77
erney.vargas <erney.vargas@mct.com.co>
2024-03-18 10:21:47 -0500

    SW_2_0-38: WIP: Finalizacion de tarea

---
+++ lib/src/globals/menu/ui/widgets/sidebar/footer_sidebar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class FooterSidebar extends StatelessWidget {
+  const FooterSidebar({
+    super.key,
+    required this.isMinimize,
+  });
+
+  final bool isMinimize;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.08,
+      padding: isMinimize ? null : const EdgeInsets.symmetric(horizontal: 54),
+      width: isMinimize ? 80 : 270,
+      color: Theme.of(context).colorScheme.primary,
+      child: isMinimize
+          ? Icon(Icons.double_arrow_outlined, color: Colors.white.withOpacity(0.3))
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.forum_outlined, color: Theme.of(context).colorScheme.onTertiary.withOpacity(0.6), size: 20),
+                Icon(Icons.telegram_sharp, color: Theme.of(context).colorScheme.onTertiary.withOpacity(0.6), size: 20),
+                Icon(Icons.call_outlined, color: Theme.of(context).colorScheme.onTertiary.withOpacity(0.6), size: 20),
+              ],
+            ),
+    );
+  }
+}
