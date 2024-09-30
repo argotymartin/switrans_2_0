c3211d7044ae20c5daf0fed2a7a12b927f2900b5
erney.vargas <erney.vargas@mct.com.co>
2024-09-30 17:13:39 -0500

    SW2-141 Totalización de Valores en la Tabla de Documentos

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -17,7 +17,7 @@ void initializeConfig() {
       kBackendBaseUrlMaestro = 'http://192.168.24.158:8084';
       break;
     case 'develop':
-      kPocketBaseUrl = 'http://192.168.102.34:8090';
+      kPocketBaseUrl = 'http://192.168.102.18:8090';
       kBackendBaseUrlERP = 'http://192.168.102.18:10000';
       kBackendBaseUrlMaestro = 'http://192.168.102.18:10000';
       break;
