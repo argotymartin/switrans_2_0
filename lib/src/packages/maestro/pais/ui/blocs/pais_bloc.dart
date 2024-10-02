c8394712379be439b5a600a4d27a94e8ecf1fb81
erney.vargas <erney.vargas@mct.com.co>
2024-10-02 15:09:13 -0500

    WIP: Refactorizacion en pais

--- lib/src/packages/maestro/pais/ui/blocs/pais_bloc.dart
+++ lib/src/packages/maestro/pais/ui/blocs/pais_bloc.dart
@@ -54,19 +54,19 @@ class PaisBloc extends Bloc<PaisEvent, PaisState> {
       event.requestList.map((PaisRequest request) => _repository.updatePaisService(request)),
     );
 
-    final List<Pais> paiss = <Pais>[];
+    final List<Pais> paises = <Pais>[];
     final List<DioException> exceptions = <DioException>[];
 
     for (final DataState<Pais> dataState in dataStateList) {
       if (dataState.data != null) {
-        paiss.add(dataState.data!);
+        paises.add(dataState.data!);
       } else if (dataState.error != null) {
         exceptions.add(dataState.error!);
       }
     }
 
-    if (paiss.isNotEmpty) {
-      emit(state.copyWith(status: PaisStatus.consulted, paises: paiss));
+    if (paises.isNotEmpty) {
+      emit(state.copyWith(status: PaisStatus.consulted, paises: paises));
     }
 
     if (exceptions.isNotEmpty) {
