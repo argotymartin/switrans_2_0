db99cc6598cfbae3b4451b544f97860bb29f2bcc
erney.vargas <erney.vargas@mct.com.co>
2024-10-08 16:14:43 -0500

    SW2-147 Mejorar la Navegación en las Páginas de Registro

--- lib/src/config/routers/financiero_routes.dart
+++ lib/src/config/routers/financiero_routes.dart
@@ -1,48 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:switrans_2_0/injector.dart';
 import 'package:switrans_2_0/src/config/routers/validate_routes.dart';
-import 'package:switrans_2_0/src/globals/menu/ui/layouts/menu_layout.dart';
 import 'package:switrans_2_0/src/packages/financiero/factura/ui/factura_ui.dart';
 import 'package:switrans_2_0/src/packages/financiero/factura/ui/views/search/factura_search_view.dart';
 
 class FinancieroRoutes {
   static const String packagePath = "/financiero";
 
-  static List<ShellRoute> getRoutesFinaciero() {
-    final List<ShellRoute> routes = <ShellRoute>[];
-    routes.add(routerFactura());
-    return routes;
+  static List<GoRoute> getRoutesFinanciero() {
+    return <GoRoute>[
+      ..._routerFactura(),
+    ];
   }
 
-  static ShellRoute routerFactura() {
+  static List<GoRoute> _routerFactura() {
     const String modulePath = "factura";
-    return ShellRoute(
-      builder: (BuildContext context, GoRouterState state, Widget child) {
-        return BlocProvider<FacturaBloc>(
-          create: (_) => FacturaBloc(injector())..add(const GetInitialFormFacturaEvent()),
-          child: MenuLayout(child: child),
-        );
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: "$packagePath/$modulePath/registrar",
-          builder: (BuildContext context, GoRouterState state) {
-            //context.read<FacturaBloc>().request.clean();
-            return const FacturaCreateView();
-          },
-          redirect: ValidateRoutes.onValidateAuth,
-        ),
-        GoRoute(
-          path: "$packagePath/$modulePath/buscar",
-          builder: (BuildContext context, GoRouterState state) {
-            //context.read<FacturaBloc>().request.clean();
-            return const FacturaSearchView();
-          },
-          redirect: ValidateRoutes.onValidateAuth,
-        ),
-      ],
-    );
+    return <GoRoute>[
+      GoRoute(
+        path: "$packagePath/$modulePath/registrar",
+        builder: (BuildContext context, GoRouterState state) {
+          //context.read<FacturaBloc>().request.clean();
+          return const FacturaCreateView();
+        },
+        redirect: ValidateRoutes.onValidateAuth,
+      ),
+      GoRoute(
+        path: "$packagePath/$modulePath/buscar",
+        builder: (BuildContext context, GoRouterState state) {
+          //context.read<FacturaBloc>().request.clean();
+          return const FacturaSearchView();
+        },
+        redirect: ValidateRoutes.onValidateAuth,
+      ),
+    ];
   }
 }
