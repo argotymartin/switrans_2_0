cd000d973c590fc80cf878193d7471523499d3f3
erney.vargas <erney.vargas@mct.com.co>
2024-10-31 09:39:20 -0500

    SW2-160 Ajustes en la Tabla de los Visores View

--- lib/src/util/shared/widgets/tables/custom_pluto_grid/data_grid_item.dart
+++ lib/src/util/shared/widgets/tables/custom_pluto_grid/data_grid_item.dart
@@ -1,4 +1,5 @@
 import 'package:switrans_2_0/src/util/shared/models/models_shared.dart';
+import 'package:switrans_2_0/src/util/shared/widgets/inputs/text_input.dart';
 
 enum Tipo { item, text, boolean, select, date }
 
@@ -9,6 +10,10 @@ class DataItemGrid {
   final bool edit;
   final List<String>? dataList;
   final List<EntryAutocomplete>? entryMenus;
+  final int? minLength;
+  final int? maxLength;
+  final TypeInput? typeInput;
+  final double? width;
 
   DataItemGrid({
     required this.title,
@@ -17,5 +22,9 @@ class DataItemGrid {
     required this.edit,
     this.dataList,
     this.entryMenus,
+    this.minLength,
+    this.maxLength,
+    this.typeInput,
+    this.width,
   });
 }
